afe02c91881c428f8ab247853c2b3fb995705e97
Vincent Untz <vuntz@suse.com>
2014-07-21 17:16:06 +0200

    Improve migration script to multi-backend
    We really don't want to change the defaults by importing old values in
    there. We only really care about the single backend that was active, and
    that's what we import back.

--- chef/data_bags/crowbar/migrate/cinder/011_migrate_multi_backend.rb
+++ chef/data_bags/crowbar/migrate/cinder/011_migrate_multi_backend.rb
@@ -1,30 +1,32 @@
 def upgrade ta, td, a, d
-  a['volume_defaults'] = a['volume']
+  # Convert old single backend to new backend list
+  old_volume = a['volume']
   a['volume'] = []
-  backend_driver = a['volume_defaults']['volume_type']
-  # volume_type got renamed to volume_driver
-  a['volume_defaults']['volume_driver'] = a['volume_defaults']['volume_type']
-  a['volume_defaults'].delete 'volume_type'
-  # Disable Multi backend support on migration
-  a['volume_defaults']['use_multi_backend'] = false
-
+  backend_driver = old_volume['volume_type']
   a['volume'] << {
     "backend_name" => "default",
     "backend_driver" => backend_driver,
-    a['volume_defaults']['volume_driver'] => a['volume_defaults'][backend_driver]
+    backend_driver => old_volume[backend_driver]
   }
+
+  # Synchronize the defaults
+  a['volume_defaults'] = ta['volume_defaults']
+
+  # Disable Multi backend support on migration
+  a['volume_defaults']['use_multi_backend'] = false
+
   return a, d
 end
 
 
 def downgrade ta, td, a, d
   # preserve first volume backend
-  current_volume_driver = a['volume'][0]['backend_driver']
-  current_volume = a['volume'][0][current_volume_driver]
+  current_backend_driver = a['volume'][0]['backend_driver']
+  current_volume = a['volume'][0][current_backend_driver]
   a['volume'] = a['volume_defaults']
-  a['volume'][current_volume_driver] = current_volume
-  # Rename volume_driver back to volume_type
-  a['volume']['volume_type'] = current_volume_driver
+  a['volume'][current_backend_driver] = current_volume
+  # Rename backend_driver back to volume_type
+  a['volume']['volume_type'] = current_backend_driver
   a['volume'].delete 'volume_driver'
   a['volume'].delete 'use_multi_backend'
   a.delete 'volume_defaults'
